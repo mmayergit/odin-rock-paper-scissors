2cf22f2cd9c5afd99b9a51b2a79474969b51d868
mmayergit <106104728+mmayergit@users.noreply.github.com>
2023-10-21 10:50:13 +0200

    Add first draft of rock-paper-scissors.js

---
+++ rock-paper-scissors.js
@@ -0,0 +1,51 @@
+// get random choice of computer
+function getComputerChoice() {
+     let choices = ['rock', 'paper', 'scissors'];
+    return choices[Math.floor(Math.random()*3)];
+}
+
+// get Player selection
+function getPlayerChoice() {
+    return prompt("Choose one of 'Rock', 'Paper', or 'Scissors': ");
+}
+
+// play a single round; return winner string
+// win: rock - scissor, scissor - paper, paper - rock
+function playSingleRound(computerChoice, playerChoice) {
+    playerChoice = playerChoice.toLowerCase();
+    let winnerString;
+    if (computerChoice === playerChoice) {
+        winnerString = "It's a tie!";
+    } else if (computerChoice === "rock" && playerChoice === "scissor" ||
+                computerChoice === "scissor" && playerChoice === "paper" ||
+                computerChoice === "paper" && playerChoice === "rock") {
+        winnerString = `You lose! ${computerChoice} beats ${playerChoice}`;
+    } else {
+        winnerString = `You win! ${playerChoice} beats ${computerChoice}`;
+    }
+    return winnerString;
+}
+
+// play a game: call singleRound 5 times; return overall winner
+function playGame() {
+    let playerWins = 0;
+    let computerWins = 0;
+    for (let i = 1; i <= 5; i++) {
+        winMessage = playSingleRound(getComputerChoice(), getPlayerChoice());
+        console.log(winMessage);
+        if (winMessage.slice(4, 7) === "win") {
+            playerWins++;
+        } else if(winMessage.slice(4, 8) === "lose") {
+            computerWins++;
+        }
+    }
+    if (playerWins > computerWins) {
+        console.log("You win!");
+    } else if (computerWins > playerWins) {
+        console.log("You lose!");
+    } else {
+        console.log("It's a tie!");
+    }
+}
+
+playGame();
